dbe425533c1cf78e4d2de28e6e2c1dd57e28a3a5
Liokima <mr.03grigas@gmail.com>
2024-10-12 10:54:27 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,4 @@
+Homework_Completed = "12"
+Hours_on_Homework = "1.5"
+Name_Course = "Python"
+print('Наименование курса -',Name_Course,';', 'Кол-во задач -',Homework_Completed,';', 'Затрачено часов -', Hours_on_Homework,';', 'Среднее время выполнения задачи -', (float(Hours_on_Homework)) / (int(Homework_Completed)))
