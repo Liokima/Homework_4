eeebc833e6b5339a2f0241ebc24397cb01f31fea
Liokima <mr.03grigas@gmail.com>
2024-10-12 12:17:11 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Напишите любое словосочетание: ')
+print('Кол-во символов: ',len(my_string))
+print('UP -',my_string.upper())
+print('low -',my_string.lower())
+print(my_string.replace(' ',''))
+
+print('Первая буква: ', my_string[0])
+print('Последняя буква: ', my_string[-1])
